82f41c97e11c4c3bcbce5259a322c768281b2142
Jan Hensel <ja_he@uni-bremen.de>
2017-03-02 17:07:28 +0100

    wacom scripts for eDP1 and HDMI

---
+++ .wacomhdmi.sh
@@ -0,0 +1,5 @@
+xsetwacom set "Wacom Intuos Pro S Pen stylus" MapToOutput HDMI1
+xsetwacom set "Wacom Intuos Pro S Pen eraser" MapToOutput HDMI1
+
+xsetwacom set "Wacom Intuos Pro S (WL) Pen stylus" MapToOutput HDMI1
+xsetwacom set "Wacom Intuos Pro S (WL) Pen eraser" MapToOutput HDMI1
