543db980cb1f92daccff826133513ab04d40d2d8
Jan Hensel <ja_he@uni-bremen.de>
2017-12-31 15:37:58 +0100

    inputrc

--- .dotfilecollect.sh
+++ .dotfilecollect.sh
@@ -4,6 +4,7 @@ cp ~/.vimrc                 ~/gits/dotfiles/.vimrc
 cp ~/.config/i3/config      ~/gits/dotfiles/configi3
 cp ~/.config/rofi/config    ~/gits/dotfiles/configrofi
 cp ~/.templates/*           ~/gits/dotfiles/.templates/
+cp ~/.inputrc               ~/gits/dotfiles/.inputrc
 
 cd ~/gits/dotfiles
 git status
