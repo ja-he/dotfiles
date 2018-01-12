afe42766a07a2ac03a84640644399f9160356c04
Jan Hensel <ja_he@uni-bremen.de>
2018-01-12 23:56:05 +0100

    added 1sec wait

--- .scripts/lockimg.sh
+++ .scripts/lockimg.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sleep 1
 scrot ~/.scripts/variables/shot.png
 convert -scale 10% ~/.scripts/variables/shot.png -scale 1000% ~/.scripts/variables/pixl.png
 rm ~/.scripts/variables/shot.png
