8e6f952e450ccbacb712f7cdd8719960fd6383c7
Jan Hensel <ja_he@uni-bremen.de>
2018-02-24 21:48:43 +0100

    havent pushed in a while

--- .scripts/dualscreen.sh
+++ .scripts/dualscreen.sh
@@ -2,5 +2,5 @@
 # xrandr 
 xrandr --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output eDP1 --primary --mode 1366x768 --pos 2136x416 --rotate normal --output VIRTUAL1 --off
 
-# feh 
-feh --bg-fill ~/.bgimg.png
+# wal 
+wal -b "#000000" -R
