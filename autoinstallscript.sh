2a03a0eda907d72b23471a455181d03f92f35e58
Jan Hensel <ja_he@uni-bremen.de>
2018-10-03 22:00:35 +0200

    Add installation of Iosevka to the automatic installations

--- autoinstallscript.sh
+++ autoinstallscript.sh
@@ -163,6 +163,10 @@
     sudo pip3 install pywal 
 # bluetooth
     sudo apt-get install bluez-tools -y 
+# iosevka font 
+    sudo add-apt-repository ppa:laurent-boulard/fonts
+    sudo apt-get update 
+    sudo apt-get install fonts-iosevka
 # scientifica font 
     cd /etc/fonts/conf.d/
     sudo rm /etc/fonts/conf.d/10* && sudo rm -rf 70-no-bitmaps.conf && sudo ln -s ../conf.avail/
