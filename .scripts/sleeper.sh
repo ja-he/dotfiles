a6cfd2cb8219fdbc0dfc558ac4b96bc0bf0dc59f
Jan Hensel <ja_he@uni-bremen.de>
2018-12-29 15:56:43 +0100

    Overhaul touchpad setup script

--- .scripts/sleeper.sh
+++ .scripts/sleeper.sh
@@ -4,7 +4,7 @@ while true; do
     if [ "$(date +%H%M)" == "2100" ]; then 
         shutdown 4 
         // echo "shutting down in four minutes..." | festival --tts --pipe 
-        mpv ~/Music/_conv/David\ Bowie\ -\ Life\ On\ Mars-v--IqqusnNQ.webm.mp3 --volume=60 
+        mpv ~/Music/_conv/bowie_lifeonmars.mp3 --volume=60 --geometry=100x100+5+5
     else 
         echo -ne "date is: "
         date +%H%M
--- .scripts/trackpadsettings.sh
+++ .scripts/trackpadsettings.sh
@@ -1,22 +1,59 @@
 #!/bin/bash
 
+
+function print_help {
+    echo "USAGE:    turn [off],                 " 
+    echo "          turn [on],                  " 
+    echo "          [initialize] to defaults,   " 
+    echo "          get [help]                  "
+}
+
+if [ -z $1 ]; then 
+    echo "PLEASE ENTER AN ACTION:"  
+    print_help
+    exit 
+fi 
+
+if [ "$1" = "help" ]; then 
+    print_help
+    exit 
+fi 
+
 # get the device id of the touchpad 
-    TOUCHPAD_ID="$(xinput --list | grep Synaptics | grep -oP 'id=\K.*?(?=\s)')"
-    echo $TOUCHPAD_ID
+TOUCHPAD_ID="$(xinput --list | grep Synaptics | grep -oP 'id=\K.*?(?=\s)')"
+echo "found touchpad ID: $TOUCHPAD_ID"
 
 
-# turn tap-to-click off
+if [ "$1" = "off" ]; then 
+    xinput set-prop $TOUCHPAD_ID "Device Enabled" 0
+    echo "turned off :)"
+    exit
+fi 
+
+if [ "$1" = "on" ]; then 
+    xinput set-prop $TOUCHPAD_ID "Device Enabled" 1
+    echo "turned on :)"
+    exit
+fi 
+
+if [ "$1" = "initialize" ]; then 
+    xinput set-prop $TOUCHPAD_ID "Device Enabled" 1
+
+    # turn tap-to-click off
     #xinput set-prop $TOUCHPAD_ID 293 0 
     xinput set-prop $TOUCHPAD_ID "Synaptics Tap Action" 0 
 
-# turn acceleration off 
+    # turn acceleration off 
     #xinput set-prop $TOUCHPAD_ID 273 0 
     xinput set-prop $TOUCHPAD_ID "Device Accel Profile" 0 
 
-# set two-finger-scrolling to vertical only 
+    # set two-finger-scrolling to vertical only 
     #xinput set-prop $TOUCHPAD_ID 288 1 0 
     xinput set-prop 13 "Synaptics Two-Finger Scrolling" 1 0
 
-# set edge-scrolling to horizontal only 
+    # set edge-scrolling to horizontal only 
     #xinput set-prop $TOUCHPAD_ID 287 0 1 0 
     xinput set-prop $TOUCHPAD_ID "Synaptics Edge Scrolling" 0 1 0 
+    echo "turned initialized :)"
+    exit
+fi 
