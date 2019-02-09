5bdf812136292663d4bfb399fa1d44b20eef534f
Jan Hensel <ja_he@uni-bremen.de>
2019-02-09 11:10:44 +0100

    Gerify customres-script

--- .scripts/customres.sh
+++ .scripts/customres.sh
@@ -2,20 +2,29 @@
 
 # checking for help prompt
 if [ "$1" == "-h" ]; then 
-    echo -e "\t\tUsage: \$ customres.sh <width> <height>"
-    echo -e "\t\ti.E.:  \$ customres.sh  800     450"
+    echo -e "\t\tUsage: \$ customres.sh <monitor>   <width> <height>"
+    echo -e "\t\ti.E.:  \$ customres.sh LVDS-1      800     450"
+    exit 0 
 fi 
 
-# checking for existance of parameters 
+monitor="$1"
 if [ -z $1 ]; then 
+    echo "you didnt enter a monitor"
+    exit 1
+fi
+
+# checking for existance of parameters 
+if [ -z $2 ]; then 
+    echo "you didnt enter a width"
     exit 1
 fi 
-if [ -z $2 ]; then 
+if [ -z $3 ]; then 
+    echo "you didnt enter a width"
     exit 1
 fi
 
 # generating the modeline 
-MODELINE=$(cvt $1 $2 60)
+MODELINE=$(cvt $2 $3 60)
 MODELINE=$(echo -n '"'${MODELINE#*\"})
 
 # creating the mode
@@ -23,7 +32,7 @@ xrandr --newmode $MODELINE
 RESNAME=${MODELINE%% *}
 
 # adding the mode to the display's modes
-xrandr --addmode LVDS-1 $RESNAME 
+xrandr --addmode $monitor $RESNAME 
 
 # changing to the custom resolution 
-xrandr --output LVDS-1 --mode $RESNAME
+xrandr --output $monitor --mode $RESNAME
