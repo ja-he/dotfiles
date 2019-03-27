ccb527022fd80f6ec08533e930f1a9f282f2cc6c
Jan Hensel <ja_he@uni-bremen.de>
2019-03-27 09:16:03 +0100

    Add a battery warning script
    This script notifies the user with a notification at 5% charge and -if
    after notifying the battery goes to 2% - puts the laptop to sleep to
    prevent data loss.

--- .scripts/batterywarner.sh
+++ .scripts/batterywarner.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
+warned=false
 while (true); do 
+
     # wait 60 seconds 
     sleep 60
+
     # get battery percentage as a single number
     original_output=$(acpi)
     dropped_clutter_prefix=${original_output#*,}
     dropped_clutter_suffix=${dropped_clutter_prefix%'%'*}
+
     # this now holds the iE "24" if battery is 24% charged
     percentage_digits=$dropped_clutter_suffix
+
+    # current battery level printout 
+    echo "current battery level: $percentage_digits%"
+
+    # if the battery percentage is at 2% or less and the user has already been 
+    # warned, then suspend the machine to prevent loss of power.
+    if (( $percentage_digits <= 2 )); then 
+        if $warned; then 
+            warned=false
+            systemctl-suspend
+        fi
+    fi 
+
     # if the battery percentage is below 5%, put up a warning
     if (( $percentage_digits <= 5 )); then 
-        i3-nagbar -t WARNING -m "CHARGE YOUR LAPTOP, DUMMY!"
+        notify-send "Your battery is almost empty." \
+            "The battery is at $percentage_digits%, so you should charge it. :)" \
+            --urgency=critical \
+            --icon=/usr/share/icons/Numix/48/notifications/notification-battery-low.svg
+        warned=true
     fi 
+
 done 
