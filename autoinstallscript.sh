08f397bc312b71b5022c4ec20343348249ef72ce
Jan Hensel <ja_he@uni-bremen.de>
2019-06-09 10:45:26 +0200

    Remove i3-gaps installation

--- autoinstallscript.sh
+++ autoinstallscript.sh
@@ -116,19 +116,6 @@
     cd ~/.vim/bundle
     git clone https://github.com/ervandew/supertab.git 
     cd
-# i3-gaps [TODO: does this work? i don't believe it does...]
-    # dependencies 
-        sudo apt-get install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev -y
-    # install 
-        cd ~/gits
-        git clone https://www.github.com/Airblader/i3 i3-gaps
-        cd i3-gaps
-        autoreconf --force --install
-        rm -rf build/
-        mkdir -p build && cd build/
-        ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
-        make
-        sudo make install
 # arc-theme
     sudo apt-get install arc-theme -y
 # numix
