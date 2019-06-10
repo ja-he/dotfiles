7ee1603705f67cd12989fd5c1b778e08574d530c
Jan Hensel <ja_he@uni-bremen.de>
2019-06-10 11:59:21 +0200

    Bugfix trackpad initialization script

--- .scripts/trackpadsettings.sh
+++ .scripts/trackpadsettings.sh
@@ -49,11 +49,11 @@ if [ "$1" = "initialize" ]; then
 
     # set two-finger-scrolling to vertical only 
     #xinput set-prop $TOUCHPAD_ID 288 1 0 
-    xinput set-prop 13 "Synaptics Two-Finger Scrolling" 1 0
+    xinput set-prop $TOUCHPAD_ID "Synaptics Two-Finger Scrolling" 1 0
 
     # set edge-scrolling to horizontal only 
     #xinput set-prop $TOUCHPAD_ID 287 0 1 0 
     xinput set-prop $TOUCHPAD_ID "Synaptics Edge Scrolling" 0 1 0 
-    echo "turned initialized :)"
+    echo "initialized :)"
     exit
 fi 
