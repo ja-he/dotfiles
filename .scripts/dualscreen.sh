25dc1dd355d133d1fbe8873aa264d0c8bd127d6d
Jan Hensel <ja_he@uni-bremen.de>
2019-12-14 14:22:02 +0100

    Update screen scripts

--- .scripts/dualscreen.sh
+++ .scripts/dualscreen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-1 --mode 1920x1080 --pos 1600x952 --rotate normal --output LVDS-1 --primary --mode 1600x900 --pos 0x1132 --rotate normal --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off
+xrandr --output LVDS1 --primary --mode 1600x900 --pos 0x352 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VGA1 --mode 1920x1080 --pos 1600x0 --rotate normal --output VIRTUAL1 --off
--- .scripts/singlescreen.sh
+++ .scripts/singlescreen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-1 --off --output eDP-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off
+xrandr --output LVDS1 --primary --mode 1600x900 --pos 0x352 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VGA1 --off --output VIRTUAL1 --off
