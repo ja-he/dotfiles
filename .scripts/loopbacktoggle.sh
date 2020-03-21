94f3c8dfcb6de2f9f6ddf17a4c28b776433b87cd
Jan Hensel <ja_he@uni-bremen.de>
2020-03-21 14:42:34 +0100

    Add toggles for mic mute and loopback
    Careful: this works on the basis of variables (files) in a subfolder
             '.vars'.

---
+++ .scripts/loopbacktoggle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+varfile='/home/ztf/.scripts/.vars/lbt'
+
+if [[ $(cat "${varfile}") == *on* ]]; then
+  echo -n 'off' > "${varfile}"
+  pacmd unload-module module-loopback
+  notify-send -t 1000 "loopback off"
+else 
+  echo -n 'on' > "${varfile}"
+  pacmd load-module module-loopback
+  notify-send -t 1000 "loopback on"
+fi
---
+++ .scripts/micmutetoggle.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# the name of the device to be muted
+devname='alsa_input.usb-C-Media_Electronics_Inc._USB_Advanced_Audio_Device-00.analog-stereo'
+
+mutestatusfile='/home/ztf/.scripts/.vars/mic'
+
+# This statement simply checks whether the FIRST(!) source found is
+# muted by checking whether the string contains the substring "yes"
+# This is the quick-and-dirty solution and it relies on it being the
+# first input device. If this is not the case, simply replug the
+# higher-ranked devices until it is the first. (or improve the
+# script i guess...) 
+if [[ "$(cat "${mutestatusfile}")" == *hot* ]]; then 
+  echo "mic was hot"
+  # flash a short notification
+  notify-send -t 1000 "mic muted" & 
+  # play the unplug sound 
+  mpv /usr/share/sounds/freedesktop/stereo/device-removed.oga & 
+  pactl set-source-mute "${devname}" true
+  echo "muted" > "${mutestatusfile}"
+else 
+  # flash a short notification
+  notify-send -t 1000 "mic hot" & 
+  # play the unplug sound 
+  mpv /usr/share/sounds/freedesktop/stereo/device-added.oga & 
+  pactl set-source-mute "${devname}" false
+  echo "hot" > "${mutestatusfile}"
+fi
