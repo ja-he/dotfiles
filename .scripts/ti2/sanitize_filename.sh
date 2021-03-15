33be49e7aebf4be65109ee89c1a73984922d564c
Jan Hensel <ja_he@uni-bremen.de>
2021-03-15 22:40:18 +0100

    scripts::ti2: also sanitize exercise name (e.g. Bonus)

--- .scripts/ti2/sanitize_filename.sh
+++ .scripts/ti2/sanitize_filename.sh
@@ -4,9 +4,10 @@ function sanitize_filename() {
   filename="${1}"
   # TODO: verify filename
 
-  prefix=$(echo ${filename} | cut -d'_' -f1,2 )
+  ex=$(echo ${filename} | cut -d'_' -f1 | tr [A-Z] [a-z])
+  group=$(echo ${filename} | cut -d'_' -f2)
   names=$(echo ${filename} | cut -d'_' -f3- | tr [A-Z] [a-z])
-  echo ${prefix}_${names}
+  echo ${ex}_${group}_${names}
 }
 
 while read filename; do
