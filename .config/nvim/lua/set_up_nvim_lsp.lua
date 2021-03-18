0ed202a36cbdd2a7b72f538ef4549b05c6f491eb
Jan Hensel <ja_he@uni-bremen.de>
2021-03-18 20:12:30 +0100

    nvim::lsp: don't update diagnostics while typing
    I changed how I highlight the LSP diagnostics, de-emphasizing the
    diagnostic text and emphasizing instead highlighting the word the
    diagnostics take issue with (e.g. the mistyped identifier).
    This would cause an annoying flickering of the typed text, if the
    diagnostics kept updating after every keystroke in insert mode.
    I'd hope that highlighting the word in this way makes finding errors
    -- especially typos -- more speedy.

--- .config/nvim/lua/set_up_nvim_lsp.lua
+++ .config/nvim/lua/set_up_nvim_lsp.lua
@@ -3,7 +3,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     virtual_text = true,
     signs = false,
-    update_in_insert = true,
+    update_in_insert = false,
   }
 )
 
