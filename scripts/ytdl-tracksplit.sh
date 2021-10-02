5f95c1bf5af2f6f59ce720c61dee4981177ad348
Jan Hensel <ja_he@uni-bremen.de>
2021-10-02 23:11:17 +0200

    scripts::ytdl-tracksplit: Rewrite

--- scripts/ytdl-tracksplit.sh
+++ scripts/ytdl-tracksplit.sh
@@ -57,6 +57,30 @@ get_filename() {
   echo "${tracknum} - ${title}.mp3"
 }
 
+filter_timestamps() {
+  grep -P '[0-9]{1,2}:[0-9]{2}'
+}
+
+reformat_timestamps() {
+  while IFS='|' read timestamp title
+  do
+    if [[ -n ${prev_timestamp} ]]
+    then
+      echo "${prev_timestamp}|${timestamp}|${prev_title}"
+    fi
+    prev_timestamp=${timestamp}
+    prev_title="${title}"
+  done
+}
+
+print_ffmpeg() {
+  while tracknum=0 IFS='|' read start end title
+  do
+    tracknum=$((${tracknum}+1))
+    echo "ffmpeg -ss ${start} -i \"${big_file}\" -acodec copy -to ${end} \"$(get_filename ${tracknum} "${title}")\""
+  done
+}
+
 if [[ -z "$1" ]]
 then
   echo "no URL provided, exiting"
@@ -68,6 +92,23 @@ description_file=.description
 big_file='.full.mp3'
 order='timestamp-first'
 
+# get description into file
+youtube-dl --get-description ${url} > ${description_file}
+
+# check with user first
+echo "here's the commands as I would execute them:"
+(
+IFS=$'\n'
+for cmd in $(cat "${description_file}" | filter_timestamps | parse_timestamp_line_c | reformat_timestamps | print_ffmpeg); do
+  echo "${cmd}"
+done
+)
+read -p "ok? y/[n] > " input
+if [ "$input" != "y" ]; then
+  echo "aborting..."
+  exit 0
+fi
+
 # get file
 youtube-dl --extract-audio --audio-quality 0 --audio-format mp3 ${url}
 
@@ -78,34 +119,16 @@ ytdl_filename="${ytdl_filename%.mp4}"
 ytdl_filename="${ytdl_filename}.mp3"
 mv "${ytdl_filename}" ${big_file}
 
-# get description into file
-youtube-dl --get-description ${url} > ${description_file}
-
 # parse description
 prev_timestamp=''
 prev_title=''
-cat ${description_file} \
-  | grep -P '[0-9]{1,2}:[0-9]{2}' \
-  | parse_timestamp_line_b \
-  | while IFS='|' read timestamp title
-    do
-      if [[ -n ${prev_timestamp} ]]
-      then
-        echo "${prev_timestamp}|${timestamp}|${prev_title}"
-      fi
-      prev_timestamp=${timestamp}
-      prev_title="${title}"
-    done \
-  | while tracknum=0 IFS='|' read start end title
-    do
-      tracknum=$((${tracknum}+1))
-      ffmpeg \
-        -i "${big_file}" \
-        -acodec copy \
-        -ss ${start} \
-        -to ${end} \
-        "$(get_filename ${tracknum} "${title}")"
-    done
+(
+IFS=$'\n'
+for cmd in $(cat "${description_file}" | filter_timestamps | parse_timestamp_line_c | reformat_timestamps | print_ffmpeg); do
+  echo "EXECUTING '${cmd}'"
+  bash -c "${cmd}"
+done
+)
 
 # clean up
 rm ${description_file}
