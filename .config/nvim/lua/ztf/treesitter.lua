dfc2eb28ae20baf45c252e39bd163cb6ba9e05c9
Jan Hensel <ja_he@uni-bremen.de>
2022-03-12 23:46:03 +0100

    nvim: Add treesitter parser for mock language grammar / highlighting
    Note: this also requires moving the highlights.scm to
    'nvim/queries'.

--- .config/nvim/lua/ztf/treesitter.lua
+++ .config/nvim/lua/ztf/treesitter.lua
@@ -5,3 +5,17 @@ require'nvim-treesitter.configs'.setup {
     disable = { },  -- list of language that will be disabled
   },
 }
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.spdxtv = {
+  install_info = {
+    url = "~/repositories/ts-test/parser/tree-sitter-fakelang", -- local path or git repo
+    files = {"src/parser.c"},
+    -- optional entries:
+    branch = "master", -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+  },
+  filetype = "spdx", -- if filetype does not match the parser name
+  highlight = true,
+}
