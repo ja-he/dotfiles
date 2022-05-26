af0613d6ce425c41192538f3ced1b1126cb7bb6d
Jan Hensel <ja_he@uni-bremen.de>
2022-05-26 16:21:01 +0200

    nvim: Add MUD treesitter parser (from local grammar)

--- .config/nvim/lua/ztf/treesitter.lua
+++ .config/nvim/lua/ztf/treesitter.lua
@@ -19,3 +19,15 @@ parser_config.spdxtv = {
   filetype = "spdx", -- if filetype does not match the parser name
   highlight = true,
 }
+parser_config.mud = {
+  install_info = {
+    url = "~/repositories/mud-lsp/parser/tree-sitter-mud", -- local path or git repo
+    files = {"src/parser.c"},
+    -- optional entries:
+    branch = "main", -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+  },
+  filetype = "mud", -- if filetype does not match the parser name
+  highlight = true,
+}
