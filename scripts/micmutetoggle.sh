594abf18726b568f84f826306a598fe69aafb127
Jan Hensel <ja_he@uni-bremen.de>
2022-06-13 19:38:56 +0200

    scripts::micmute: Use pactl instead (for pipewire compat)

--- scripts/micmutetoggle.sh
+++ scripts/micmutetoggle.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 pulseaudio-ctl mute-input
 muted="$(pulseaudio-ctl full-status | cut -f3 -d' ')"
+pactl set-source-mute @DEFAULT_SOURCE@ toggle
+muted="$(pactl get-source-mute @DEFAULT_SOURCE@ | sed 's/Mute:\s*\(\S*\)$/\1/')"
 message="$(echo "${muted}" | sed 's/yes/mic muted/' | sed 's/no/mic hot/')"
 if [ "${muted}" == "no" ]; then
   mpv /usr/share/sounds/freedesktop/stereo/device-added.oga &
