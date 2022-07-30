6d7dfc0958b346ff2b462778a4cf980eb64f4591
Jan Hensel <ja_he@uni-bremen.de>
2022-07-30 22:17:38 +0200

    nvim: Set up Lua LSP

--- .config/nvim/lua/ztf/lsp.lua
+++ .config/nvim/lua/ztf/lsp.lua
@@ -20,12 +20,29 @@ lspconfig.bashls.setup{}
 
 lspconfig.pylsp.setup{}
 
--- local luadev = require("lua-dev").setup{
---   lspconfig = {
---     cmd = {"lua-language-server"}
---   },
--- }
--- lspconfig.sumneko_lua.setup{luadev}
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+lspconfig.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      telemetry = { enable = false, }, -- 😠
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+    },
+  },
+}
 
 lspconfig.clangd.setup{
   init_options = { clangdFileStatus = true },
