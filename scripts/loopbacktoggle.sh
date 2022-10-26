fe0818591eb386f0916fdffc7f6fad2e850f7ed6
Jan Hensel <ja_he@uni-bremen.de>
2022-10-26 21:33:41 +0200

    loopbacktoggle: Drop status files for pactl
    I think this version was the right one? Too lazy to check with and
    without mic rn.

--- scripts/loopbacktoggle.sh
+++ scripts/loopbacktoggle.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-varfile="${HOME}/.vars/lbt"
-
 _exit_because_monitor() {
     mpv /usr/share/sounds/freedesktop/stereo/dialog-error.oga > /dev/null &
     notify-send -t 1000 -u critical "default source is a monitor?"
@@ -9,12 +7,10 @@ _exit_because_monitor() {
 }
 pactl get-default-source | grep -i "monitor" && _exit_because_monitor
 
-if [[ $(cat "${varfile}") == *on* ]]; then
-  echo -n 'off' > "${varfile}"
+if pactl list | grep loopback; then
   pactl unload-module module-loopback
   notify-send -t 1000 "loopback off"
 else 
-  echo -n 'on' > "${varfile}"
   pactl load-module module-loopback
   notify-send -t 1000 "loopback on"
 fi
