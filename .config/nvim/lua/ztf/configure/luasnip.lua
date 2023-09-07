fc0b3183e849a59dc6ab8ae7aa1f6907b8af5347
Jan Hensel <ja_he@uni-bremen.de>
2023-09-07 17:49:58 +0200

    nvim: Move snippets

--- .config/nvim/lua/ztf/configure/luasnip.lua
+++ .config/nvim/lua/ztf/configure/luasnip.lua
@@ -18,3 +18,31 @@ vim.cmd('snoremap <c-h> <cmd>lua require"luasnip".jump(-1)<CR>')
 vim.cmd('imap <silent><expr> <c-j> luasnip#choice_active() ? "<Plug>luasnip-next-choice" : "<c-j>"')
 vim.cmd('smap <silent><expr> <c-j> luasnip#choice_active() ? "<Plug>luasnip-next-choice" : "<c-j>"')
 
+local s = luasnip.snippet
+-- local sn = luasnip.snippet_node
+local t = luasnip.text_node
+local i = luasnip.insert_node
+-- local f = luasnip.function_node
+-- local c = luasnip.choice_node
+-- local d = luasnip.dynamic_node
+-- local r = luasnip.restore_node
+-- local l = require("luasnip.extras").lambda
+-- local rep = require("luasnip.extras").rep
+-- local p = require("luasnip.extras").partial
+-- local m = require("luasnip.extras").match
+-- local n = require("luasnip.extras").nonempty
+-- local dl = require("luasnip.extras").dynamic_lambda
+-- local fmt = require("luasnip.extras.fmt").fmt
+-- local fmta = require("luasnip.extras.fmt").fmta
+-- local types = require("luasnip.util.types")
+-- local conds = require("luasnip.extras.conditions")
+-- local conds_expand = require("luasnip.extras.conditions.expand")
+
+luasnip.add_snippets("bash", {
+  s("definition with efault value", {
+    i(1, "varfoo"), t("=${"), i(2, "varbar"), t(":-"), i(3, "defaultval"), t("}"),
+  }),
+  s("random number (quick and dirty)", {
+    t("$(( $RANDOM % "), i(1, "modulus"), t(" + "), i(2, "base-value"), t(" ))"),
+  }),
+})
