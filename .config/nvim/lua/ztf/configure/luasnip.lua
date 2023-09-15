5234712f3bd6ef4344912bd808eda93f0ed952f1
Jan Hensel <ja_he@uni-bremen.de>
2023-09-15 10:16:36 +0200

    nvim: Add snippet to generate a goroutine inspector
    can be useful for debugging

--- .config/nvim/lua/ztf/configure/luasnip.lua
+++ .config/nvim/lua/ztf/configure/luasnip.lua
@@ -19,12 +19,12 @@ vim.cmd('imap <silent><expr> <c-j> luasnip#choice_active() ? "<Plug>luasnip-next
 vim.cmd('smap <silent><expr> <c-j> luasnip#choice_active() ? "<Plug>luasnip-next-choice" : "<c-j>"')
 
 local s = luasnip.snippet
--- local sn = luasnip.snippet_node
+local sn = luasnip.snippet_node
 local t = luasnip.text_node
 local i = luasnip.insert_node
 local f = luasnip.function_node
 -- local c = luasnip.choice_node
--- local d = luasnip.dynamic_node
+local d = luasnip.dynamic_node
 -- local r = luasnip.restore_node
 -- local l = require("luasnip.extras").lambda
 -- local rep = require("luasnip.extras").rep
@@ -60,6 +60,30 @@ luasnip.add_snippets("bash", {
 
 luasnip.add_snippets("all", {
   s("uuid", { f(uuid) }),
+})
+
+luasnip.add_snippets("go", {
+  s("goroutine-inspector", {
+    t("	// periodically check the number of goroutines, writing them to "), i(1, "<file/fifo>"), t({"", ""}),
+    t("	go func() {"), t({"",""}),
+    t("		ticker := time.NewTicker(1 * time.Second)"), t({"",""}),
+    t(""), t({"",""}),
+    t("		// open "), d(2, function(args) return sn(nil, { t(args[1]) }) end, {1}), t(" for writing"), t({"",""}),
+    t("		file, err := os.OpenFile("), d(3, function(args) return sn(nil, { t('"'), t(args[1]), t('"') }) end, {1}), t(", os.O_CREATE|os.O_WRONLY, 0644)"), t({"",""}),
+    t("		if err != nil {"), t({"",""}),
+    t("			panic(err)"), t({"",""}),
+    t("		}"), t({"",""}),
+    t(""), t({"",""}),
+    t("		for range ticker.C {"), t({"",""}),
+    t("			// print number of goroutines"), t({"",""}),
+    t("			n := runtime.NumGoroutine()"), t({"",""}),
+    t("			fmt.Fprintln(file, n)"), t({"",""}),
+    t("			if n > "), i(4, "1000"), t(" {"), t({"",""}),
+    t("				// 😨 what a bunch of goroutines 😨"), t({"",""}),
+    t("			}"), t({"",""}),
+    t("		}"), t({"",""}),
+    t("	}()"), t({"",""}),
+  })
 })
 
 require("luasnip.loaders.from_vscode").lazy_load()
