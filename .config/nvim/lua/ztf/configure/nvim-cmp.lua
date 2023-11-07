6433ecbbb3d000b854132e46493ba499f4c6f2a1
Jan Hensel <ja_he@uni-bremen.de>
2023-11-07 14:05:33 +0100

    nvim: cmp: allow autocompletion

--- .config/nvim/lua/ztf/configure/nvim-cmp.lua
+++ .config/nvim/lua/ztf/configure/nvim-cmp.lua
@@ -1,9 +1,9 @@
 local cmp = require("cmp")
 local lspkind = require("lspkind")
 cmp.setup {
-  completion = {
-    autocomplete = false,
-  },
+  -- completion = {
+  --   autocomplete = true,
+  -- },
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
