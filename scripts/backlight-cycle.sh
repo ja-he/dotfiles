9ea784bf40e7a9974f7528457d3a5c837b01c931
Jan Hensel <ceo@dubious.engineering>
2024-07-20 13:47:02 +0200

    scripts: update backlight script
    now goes up and down, no cycling
    
    requires xbacklight and whatnot, acpibacklight works

--- scripts/backlight-cycle.sh
+++ scripts/backlight-cycle.sh
@@ -5,8 +5,25 @@ set -x
 
 current=$(xbacklight -get)
 
-next=$(( $current + 10))
+# can take args 'up' or 'down'
+case $1 in
+  up)
+    next=$(( $current + 10))
+    ;;
+  down)
+    next=$(( $current - 10))
+    ;;
+  *)
+    echo "Usage: backlight-cycle.sh [up|down]"
+    exit 1
+    ;;
+esac
+
+# clamping
 if (( next > 100 )); then
+  next=100
+fi
+if (( next < 0 )); then
   next=0
 fi
 
