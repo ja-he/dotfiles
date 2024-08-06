a799c8abd99fdd65ff5841a4561d83f574ee6c6f
Jan Hensel <ceo@dubious.engineering>
2024-08-06 16:46:48 +0200

    nvim: add/update some dap stuff

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -54,12 +54,12 @@ local plugins = {
       require("ztf.lsp")
     end,
   },
-  'mfussenegger/nvim-dap',
+  {
+    'mfussenegger/nvim-dap',
+  },
   {
     "rcarriga/nvim-dap-ui",
-    lazy = true,
-    config = function() require("ztf.configure.dapui") end,
-    dependencies = { "mfussenegger/nvim-dap" },
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
   },
   -- rust
   {
@@ -80,7 +80,46 @@ local plugins = {
     'leoluz/nvim-dap-go',
     lazy = true,
     ft = 'go',
-    config = function() require("ztf.configure.dap-go") end,
+    opts = {
+      -- -- Additional dap configurations can be added.
+      -- -- dap_configurations accepts a list of tables where each entry
+      -- -- represents a dap configuration. For more details do:
+      -- -- :help dap-configuration
+      dap_configurations = {
+        {
+          -- Must be "go" or it will be ignored by the plugin
+          type = "go",
+          name = "Attach remote",
+          mode = "remote",
+          request = "attach",
+        },
+      },
+      delve = {
+        port = "38697",
+      },
+      -- -- delve configurations
+      -- delve = {
+      --   -- the path to the executable dlv which will be used for debugging.
+      --   -- by default, this is the "dlv" executable on your PATH.
+      --   path = "dlv",
+      --   -- time to wait for delve to initialize the debug session.
+      --   -- default to 20 seconds
+      --   initialize_timeout_sec = 20,
+      --   -- a string that defines the port to start delve debugger.
+      --   -- default to string "${port}" which instructs nvim-dap
+      --   -- to start the process in a random available port
+      --   port = "${port}",
+      --   -- additional args to pass to dlv
+      --   args = {},
+      --   -- the build flags that are passed to delve.
+      --   -- defaults to empty string, but can be used to provide flags
+      --   -- such as "-tags=unit" to make sure the test suite is
+      --   -- compiled during debugging, for example.
+      --   -- passing build flags using args is ineffective, as those are
+      --   -- ignored by delve in dap mode.
+      --   build_flags = "",
+      -- },
+    },
     dependencies = { 'mfussenegger/nvim-dap' },
   },
   {
@@ -90,9 +129,31 @@ local plugins = {
   },
   -- lua
   {
-    'folke/neodev.nvim',
-    opts = {},
-  }, -- formerly 'folke/lua-dev.nvim'
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { plugins = { "nvim-dap-ui" }, types = true },
+      },
+    },
+  },
+  { -- optional `vim.uv` typings
+    "Bilal2453/luvit-meta",
+    lazy = true
+  },
+  { -- optional completion source for require statements and module annotations
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, {
+        name = "lazydev",
+        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+      })
+    end,
+  },
 
   -- nushell
   {
