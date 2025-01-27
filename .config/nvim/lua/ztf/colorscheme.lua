57f9443d03b9677697163a4e3f022cbd9eaab3b9
Jan Hensel <ceo@dubious.engineering>
2025-01-27 23:04:05 +0100

    nvim: update colorscheme setting

--- .config/nvim/lua/ztf/colorscheme.lua
+++ .config/nvim/lua/ztf/colorscheme.lua
@@ -13,20 +13,32 @@ local themes = {
   },
   kurzzug = {
     mappings = get_kurzzug,
-    apply = function() require('lush').apply(get_kurzzug()) end,
+    apply = function() vim.cmd([[colo kurzzug]]) end,
+  },
+  midnight = {
+    mappings = nil,
+    apply = function() vim.cmd([[colo midnight]]) end,
   },
   mono = {
     mappings = get_mono,
-    apply = function() require('lush').apply(get_mono()) end,
+    apply = function() vim.cmd([[colo mono_lush]]) end,
   },
 }
 
-if not vim.fn.has('win32') and os.getenv("DISPLAY") == nil then
-  return themes.fahrenheit
-else
-  if os.getenv("ZTF_COLO") == 'dark' then
-    return themes.kurzzug
-  else
-    return themes.mono
-  end
-end
+return {
+  themes = themes,
+  auto = function()
+    if not vim.fn.has('win32') and os.getenv("DISPLAY") == nil then
+      return themes.fahrenheit
+    else
+      if os.getenv("ZTF_COLO") == 'dark' then
+        return themes.kurzzug
+      else
+        return themes.mono
+      end
+    end
+  end,
+  dark = function() return themes.midnight end,
+  light = function() return themes.mono end,
+  basic = function() return themes.fahrenheit end,
+}
